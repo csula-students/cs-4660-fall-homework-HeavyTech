0890be252b0c0f4b0926e4bb72d19a0be88dc9e8
Jose <j.virgen87@gmail.com>
2016-10-16 20:38:31 -0700

    quiz2

--- src/main/java/csula/cs4660/quizes/App.java
+++ src/main/java/csula/cs4660/quizes/App.java
@@ -2,6 +2,8 @@ package csula.cs4660.quizes;
 
 import csula.cs4660.quizes.models.State;
 
+import java.util.*;
+
 /**
  * Here is your quiz entry point and your app
  */
@@ -9,8 +11,45 @@ public class App {
     public static void main(String[] args) {
         // to get a state, you can simply call `Client.getState with the id`
         State initialState = Client.getState("10a5461773e8fd60940a56d2e9ef7bf4").get();
+        State endingState = Client.getState("e577aa79473673f6158cc73e0e5dc122").get();
         System.out.println(initialState);
+        System.out.println(endingState);
         // to get an edge between state to its neighbor, you can call stateTransition
         System.out.println(Client.stateTransition(initialState.getId(), initialState.getNeighbors()[0].getId()));
+
+
+
+
+
+
+    }
+
+
+    private static void BFS(State initialState, State endingState){
+
+        Queue<State> queue = new LinkedList<>();
+        Set<State>  explored = new HashSet<>();
+        Map<State,State> parent = new HashMap<>();
+        boolean status = false;
+
+        queue.add(initialState);
+
+        while(!queue.isEmpty()){
+
+            State currState = queue.poll();
+            explored.add(currState);
+
+            if(currState.getId().equals("e577aa79473673f6158cc73e0e5dc122")){
+
+                status = true;
+
+
+            }
+
+
+        }
+
+
+
     }
 }
