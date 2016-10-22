a5185741af96cd060784634bb8a4b433e5af46e9
Jose <j.virgen87@gmail.com>
2016-10-22 10:32:32 -0700

    DFS

--- src/main/java/csula/cs4660/graphs/searches/DFS.java
+++ src/main/java/csula/cs4660/graphs/searches/DFS.java
@@ -4,6 +4,8 @@ import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -13,7 +15,43 @@ public class DFS implements SearchStrategy {
     @Override
     public List<Edge> search(Graph graph, Node source, Node dist) {
 
-        //testing
-        return null;
+        return DFS(graph, source, dist, source, new HashMap<>());
+    }
+
+
+    public List<Edge> DFS(Graph graph, Node source, Node dist, Node currentNode, HashMap<Node, Node> nodeMap) {
+
+        System.out.println("------------");
+        System.out.println("----DFS-----");
+        System.out.println("------------");
+        
+        List<Edge> resultPath = new ArrayList<>();
+
+        if (currentNode.equals(dist)) {
+            Node c = dist;
+            Node parent = nodeMap.get(c);
+
+            while (!c.equals(source)) {
+                int distance = graph.distance(parent, c);
+                resultPath.add((new Edge(parent, c, distance)));
+                c = parent;
+                parent = nodeMap.get(c);
+            }
+            return resultPath;
+        }
+
+        for (Node n : graph.neighbors(currentNode)) {
+            if (!nodeMap.containsKey(n)) {
+                nodeMap.put(n, currentNode);
+                resultPath = DFS(graph, source, dist, n, nodeMap);
+            }
+            if (!resultPath.isEmpty()) {
+                System.out.println("Nothing in this list");
+            }
+
+        }
+
+        return resultPath;
+
     }
 }
