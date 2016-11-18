3c394674dae7dc0e0e3951b8ec81845cce71d39e
Jose <j.virgen87@gmail.com>
2016-11-17 18:35:52 -0800

    update

--- src/main/java/csula/cs4660/games/MiniMax.java
+++ src/main/java/csula/cs4660/games/MiniMax.java
@@ -4,9 +4,39 @@ import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
 
 public class MiniMax {
+
+/*
+function minimax(graph, sourceNode, depth, maximizingPlayer) {
+    // usually being optimized in a way to compute even before
+    // the end of game by **evaluate** function
+    if (depth = 0 || sourceNode is a leaf) {
+        return evaluate(soureNode.gameState); // return a number
+    }
+
+    if (maximizingPlayer) {
+        bestValue = Number.MAX_VALUE; // positive infinite
+        for (node in graph.neighbors(sourceNode)) {
+            value = minimax(node, graph, depth - 1, false);
+            bestValue = Math.max(bestValue, value);
+        }
+        return bestValue;
+    } else {
+        bestValue = Number.MIN_VALUE; // negative infinite
+        for (node in graph.neighbors(sourceNode)) {
+            value = minimax(node, graph, depth - 1, true);
+            bestValue = Math.min(bestValue, value);
+        }
+        return bestValue;
+    }
+}
+ */
+
     public static Node getBestMove(Graph graph, Node root, Integer depth, Boolean max) {
         // TODO: implement minimax to retrieve best move
         // NOTE: you should mutate graph and node as you traverse and update value
+
+
+
         return null;
     }
 }
